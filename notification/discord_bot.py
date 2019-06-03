24bc8f6f96a8b6bd98e67d4ace96ea1419cc59e9
lowwor <lowwor@foxmail.com>
2019-06-03 21:23:06 +0800

    feat: add discord msg rate limit

--- notification/discord_bot.py
+++ notification/discord_bot.py
@@ -1,4 +1,4 @@
-import json
+import time
 
 import requests
 
@@ -8,6 +8,7 @@ from config import config
 class DiscordBot(object):
 
     def __init__(self):
+        self.session = requests.Session()
         self.secrect = config.discord.get("secrect", '')
         self.webhook_url = config.discord.get("webhook_url", None)
 
@@ -19,7 +20,29 @@ class DiscordBot(object):
             headers = {
                 'Content-Type': 'application/json',
             }
-            result = requests.post(
-                self.webhook_url,
-                data=json.dumps(payload), headers=headers)
-            print(result)
+            rate_limited = True
+            resp = None
+
+            while rate_limited:
+                resp = self.session.post(self.webhook_url, json=payload,
+                                         headers=headers)
+
+                if resp.status_code == 429:  # Too many request
+                    time.sleep(resp.json()['retry_after'] / 1000.0)
+                    continue
+                else:
+                    rate_limited = False
+
+            if resp.status_code == 204:  # method DELETE
+                return
+
+            try:
+                resp.raise_for_status()
+            except requests.exceptions.HTTPError as err:
+                print(err)
+            else:
+                print("Payload delivered successfully, code {}.".format(resp.status_code))
+
+
+def __exit__(self, *args):
+    return self.session.close()
